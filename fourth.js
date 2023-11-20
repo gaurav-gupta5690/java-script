6af0dc6391a82cbb105955be1e8e0e437708e4b4
Gaurav Gupta <gauravgupta123223@gmail.com>
2023-11-20 15:12:16 +0530

    file update

--- fourth.js
+++ fourth.js
@@ -1,5 +1,11 @@
                              //Data Types
 
+                            
+//There are two types of Data types                             
+
+
+//1)Primitive Data types                             
+
 //NUMBER TYPES
 
 let age = 21;
@@ -16,4 +22,45 @@ let isFollow = true
 let ismarried = false
 
 //undefined
+
+let x;
+let y;
+
+//null
+
+let m = null
+let n = null
+
+//bigInt
+
+let k = BigInt("286438764");           //output
+                                       //123n
+                                       //typeof k
+                                       //bigInt
+
+
+//symbol
+
+let h = Symbol("Hello!");
+
+
+//2) Non-Primitive Data Types
+
+//Objects----> collections of values
+
+const student = {
+    fullName : "Gaurav Gupta",
+    age : 21,
+    marks : 80,
+    ispass : true,
+}
+//multiple collection
+console.log(student)
+
+//single collection types
+console.log(student.fullName) //OR //console.log(student["fullName"])
+console.log(student.age)      //OR //console.log(student["age"])
+console.log(student.marks)    //OR //console.log(student["marks"])
+console.log(student.ispass)   //OR //console.log(student["isPass"])
+
 
---
+++ practice set1.js
@@ -0,0 +1,22 @@
+//crate a const object called "product" to store information shown in the picture.....
+
+const product = {
+    productName : "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 270,
+}
+console.log(product);
+
+//create a const called "Profile" to store information shown in the picture.
+
+
+const Profile = {
+    Username : "Gauravgupta123",
+    Post : 195,
+    isFollow : true,
+    Followers : 58685,
+    Following : 4,
+    Bio : "Apna Nagpur,Ex-Microsoft,DRDO to educate someone is the highest privilege"
+}
+console.log(Profile)
