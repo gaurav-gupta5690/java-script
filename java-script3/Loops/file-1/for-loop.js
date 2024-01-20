6372a7532c334f6e6e5eb01aa4161fd7928926cd
Gaurav Gupta <gauravgupta123223@gmail.com>
2024-01-20 14:16:48 +0530

    update

---
+++ java-script3/Loops/file-1/for-loop.js
@@ -0,0 +1,91 @@
+//     1       Normal tricks
+
+// multiple times repeat
+
+// 5 times 
+
+
+// console.log("gaurav gupta");
+// console.log("gaurav gupta");
+// console.log("gaurav gupta");
+// console.log("gaurav gupta");
+// console.log("gaurav gupta");
+
+
+
+
+//     2         For Loop Tricks
+
+//Input
+
+// for (let Abcd= 1; Abcd <=5; Abcd++) {
+//     console.log("Gaurav Gupta");
+// }
+
+//Output
+
+//  Gaurav Gupta
+//  Gaurav Gupta
+//  Gaurav Gupta
+//  Gaurav Gupta
+//  Gaurav Gupta
+
+
+//Example  1
+//Calculate Sum of 1 to 5
+
+
+// Input
+
+// let sum = 0
+
+// for (let i=1; i<= 5; i++){
+//     sum = sum + i ;
+// }
+
+// console.log("sum =", sum)
+
+
+// Its Add from  0 ----> like 0+1+2+3+4+5= 15
+
+//   Output:-  sum= 15
+
+
+//Example  2
+//Calculate Sum of 1 to 5
+
+
+// Input
+
+// let sum = 3
+
+// for (let i=1; i<= 5; i++){
+//     sum = sum + i ;
+// }
+
+// console.log("sum =", sum)
+
+
+// Its Add from 3 ---> like 3+1+2+3+4+5= 15
+
+//   Output:-  sum= 15
+
+
+//Example  2
+//Calculate Sum of 1 to n
+
+
+// Input
+
+let sum = 3
+let n = 2
+for (let i=1; i<= n; i++){
+    sum = sum + i ;
+}
+
+console.log("sum =", sum)
+
+
+// Its Add from 3 ---> like 3+1+2 = 6
+
+//   Output:-  sum= 15
---
+++ java-script3/Loops/while-loop.js
@@ -0,0 +1,9 @@
+// while(condition){
+    //do some work
+//  }
+
+
+let i = 1
+while(i<=5){
+    console.log("Gaurav Gupta"); i++;
+}
